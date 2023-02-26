090221e488d9979be272f587a48431c4fbb31568
CeciliaZL <cecilialeonfi@gmail.com>
2023-02-26 13:38:50 -0600

    Menú de opciones

--- Prueba_codificacion_bitwise.py
+++ Prueba_codificacion_bitwise.py
@@ -1,4 +1,29 @@
-import math
+import math 
+
+"""Program title"""
+print("\t\t************")
+print("\t\t\t Codificador y decodificador")
+print("\t\t************")
+print("\n Cervantes García Eduardo\n Chávez Villanueva Giovanni Salvador\n Rosales Piña Alejandra\n Sánchez Mendoza Carlos Omar\n Torres Bravo Cecilia\n Zurita León Dana Cecilia")
+
+"""There is a menu for the user to enter the option to perform
+To choose the option a function was made where depending on the option is what is done """
+print("")
+print("Menú")
+
+print("1. Codificar")
+print("2. Decodificar")
+opcion = input("Ingrese el número de su opción: ")
+
+def menu():
+  if opcion == "1":
+    print("Codificación")
+  elif opcion == "2":
+    print("Decodificación ")
+  else:
+    print("Decodificación")
+
+menu()
 
 txt_prueba = input()
 a = int(input())
@@ -24,10 +49,3 @@ for i in range(len(txt_codec)):
     decodec.append(ord(txt_codec[i])^decodec_a)
     decodec_a = decodec_a + b
     
-
-#Imprimir decodificado
-txt_decodec = ""
-for l in decodec:
-    txt_decodec = txt_decodec + chr(l)
-print(txt_decodec)
-
